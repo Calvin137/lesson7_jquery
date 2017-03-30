64b6d57d1d61b7cd5eaaa81c55b830e3c0f91e49
Calvin Dominguez <cd16j@my.fsu.edu>
2017-03-30 03:01:04 +0000

    Lesson 7 final commit

---
+++ Lesson7_MyRecipe/README.md
@@ -0,0 +1,21 @@
+## Project Name:  Recipe Display Application
+
+### Course Title:
+LIS 2360:  Web Application Development
+
+### Assignment Date:  
+March 31, 2017
+
+### Student Name:  
+Calvin Dominguez-Lay
+
+### Project Description:
+Project introduces jQuery framework
+
+### View Project:
+https://calvin137.github.io/lesson7_jquery/
+
+### Lessons Learned in the Assignment:
+1. DOM Transversal
+2. DOM Manipulation
+3. Modifying HTML elements
---
+++ Lesson7_MyRecipe/myRecipe.js
@@ -0,0 +1,26 @@
+/*
+Program Name: Recipe Display Application
+Author: Calvin Dominguez-Lay
+Date: March 29, 2017
+Filename: myRecipe.js
+*/
+
+//displays the next element after the current target
+function display(event) {
+    $(event.currentTarget).next().fadeIn("slow")
+}//end of display
+
+//displays and animates the next element after the current target
+function display2(event) {
+ $(event.currentTarget).next().animate( {height: 'toggle'}, "slow");
+}//end of display2
+
+//change the background color h3 element when mouse hovers over it
+$("h3").hover(function(){
+    $(this).css("background-color", "white");
+    }, function(){
+    $(this).css("background-color", "purple");
+});
+
+//hover() will trigger display2 method each time mouse hovers over header
+$("h3").hover(display2);
